25ebb885b6feb83e0c6c6975a26d77930312469a
0xanonymeow <anonymeow@incatni.to>
2023-05-05 22:44:31 +0700

    feat: add remove button

--- src/pages/todo-list/index.tsx
+++ src/pages/todo-list/index.tsx
@@ -9,16 +9,23 @@ const TodoList = () => {
     setItem('')
   }
 
+  const onRemove = (index: number) => {
+    setList((list) => list.slice(0, index).concat(list.slice(index + 1, list.length)))
+  }
+
   return (
     <div className="container">
       <div className="flex gap-2">
-        <input className="p-2" value={item} onChange={(e) => setItem(e.target.value)} />
+        <input className="p-2" value={item} onChange={(e) => setItem(e.target.value)} autoFocus />
         <button onClick={onAdd}>Add</button>
       </div>
       <ul className="mt-4 flex flex-col items-center gap-2">
         {list.map((item, i) => (
-          <div key={i} className="w-full">
+          <div key={i} className="w-full flex justify-center">
             {item}
+            <div className="ml-2 text-red-500 hover:cursor-pointer" onClick={() => onRemove(i)}>
+              X
+            </div>
           </div>
         ))}
       </ul>
