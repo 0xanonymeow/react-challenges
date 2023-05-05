629f2a243a67bac34af7c8f12121d4e3e057dbdf
0xanonymeow <anonymeow@incatni.to>
2023-05-05 22:51:29 +0700

    feat: update home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,28 @@
 import { Link } from 'react-router-dom'
 
+const pages = import.meta.glob('./**/*.tsx', { eager: true })
+const routes = []
+for (const path of Object.keys(pages)) {
+  const fileName = path.match(/\.\/(.*)\.tsx$/)?.[1]
+  if (!fileName) {
+    continue
+  }
+
+  const normalizedPathName = fileName.includes('$') ? fileName.replace('$', ':') : fileName.replace(/\/index/, '')
+
+  routes.push({
+    path: fileName === 'index' ? '/' : `/${normalizedPathName.toLowerCase()}`,
+  })
+}
+
 const Home = () => {
   return (
     <div className="container flex flex-col">
-      <Link to="six-boxes">Six Boxes</Link>
-      <Link to="autocomplete">Autocomplete</Link>
-      <Link to="timer">Timer</Link>
-      <Link to="todo-list">Todo List</Link>
+      {routes.map(({ path }, i) => (
+        <Link key={i} to={path}>
+          {path}
+        </Link>
+      ))}
     </div>
   )
 }
